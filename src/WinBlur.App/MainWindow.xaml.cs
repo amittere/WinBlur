0c3b4a2708d7cb299adbc89adbc759b2cfba5cac
Andrew Mittereder <andrew.mittereder@gmail.com>
2023-11-12 17:19:54 -0800

    User/amittere/mica (#4)
    * Switch Mica to use WASDK's SystemBackdrop feature, new in 1.3.
    
    * Enable IL trimming

--- src/WinBlur.App/MainWindow.xaml.cs
+++ src/WinBlur.App/MainWindow.xaml.cs
@@ -17,24 +17,12 @@ namespace WinBlur.App
         public Frame RootFrame { get { return PageFrame; } }
         public TestModeHelper TestModeHelper { get { return App.TestModeHelper; } }
 
-        WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See below for implementation.
-        MicaController m_backdropController;
-        SystemBackdropConfiguration m_configurationSource;
-        bool micaSupported = false;
-
         public MainWindow()
         {
             this.InitializeComponent();
 
             ExtendsContentIntoTitleBar = true;
             SetTitleBar(AppTitleBar);
-
-            micaSupported = TrySetSystemBackdrop();
-            if (micaSupported)
-            {
-                // Set background to transparent so the Mica shows through
-                ContentRoot.Background = null;
-            }
 
             PersistenceId = "WinBlur.App.MainWindow";
 
@@ -47,18 +35,10 @@ namespace WinBlur.App
             if (args.WindowActivationState == WindowActivationState.Deactivated)
             {
                 AppTitleText.Foreground = (SolidColorBrush)Application.Current.Resources["WindowCaptionForegroundDisabled"];
-                if (micaSupported)
-                {
-                    AppTitleBar.Background = (SolidColorBrush)Application.Current.Resources["TitlebarBackgroundDisabled"];
-                }
             }
             else
             {
                 AppTitleText.Foreground = (SolidColorBrush)Application.Current.Resources["WindowCaptionForeground"];
-                if (micaSupported)
-                {
-                    AppTitleBar.Background = (SolidColorBrush)Application.Current.Resources["TitlebarBackground"];
-                }
             }
         }
 
@@ -70,71 +50,6 @@ namespace WinBlur.App
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        bool TrySetSystemBackdrop()
-        {
-            if (MicaController.IsSupported())
-            {
-                m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
-                m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
-
-                // Create the policy object.
-                m_configurationSource = new SystemBackdropConfiguration();
-                Activated += Window_Activated;
-                this.Closed += Window_Closed;
-                ((FrameworkElement)Content).ActualThemeChanged += Window_ThemeChanged;
-
-                // Initial configuration state.
-                m_configurationSource.IsInputActive = true;
-                SetConfigurationSourceTheme();
-
-                m_backdropController = new MicaController();
-    
-                // Enable the system backdrop.
-                // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
-                m_backdropController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
-                    m_backdropController.SetSystemBackdropConfiguration(m_configurationSource);
-                return true; // succeeded
-            }
-
-            return false; // Mica is not supported on this system
-        }
-
-        private void Window_Activated(object sender, WindowActivatedEventArgs args)
-        {
-            m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
-        }
-
-        private void Window_Closed(object sender, WindowEventArgs args)
-        {
-            // Make sure any Mica/Acrylic controller is disposed
-            // so it doesn't try to use this closed window.
-            if (m_backdropController != null)
-            {
-                m_backdropController.Dispose();
-                m_backdropController = null;
-            }
-            Activated -= Window_Activated;
-            m_configurationSource = null;
-        }
-
-        private void Window_ThemeChanged(FrameworkElement sender, object args)
-        {
-            if (m_configurationSource != null)
-            {
-                SetConfigurationSourceTheme();
-            }
-        }
-
-        private void SetConfigurationSourceTheme()
-        {
-            switch (((FrameworkElement)Content).ActualTheme)
-            {
-                case ElementTheme.Dark: m_configurationSource.Theme = SystemBackdropTheme.Dark; break;
-                case ElementTheme.Light: m_configurationSource.Theme = SystemBackdropTheme.Light; break;
-                case ElementTheme.Default: m_configurationSource.Theme = SystemBackdropTheme.Default; break;
-            }
         }
     }
 }
